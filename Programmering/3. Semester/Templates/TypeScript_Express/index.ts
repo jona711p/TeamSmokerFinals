3891382c32e1db2e76d4eabf6ffbbab6c72f057c
Jonas Lykke <jona711p@student.eadania.dk>
2018-01-09 13:46:34 +0100

    MVC & TypeScript
    Tomt MVC, MVC med Templates & TypeScript med Express

---
+++ Programmering/3. Semester/Templates/TypeScript_Express/index.ts
@@ -0,0 +1,22 @@
+// Imports
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+
+// Init Express Server
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Init & Start Server
+let port = (process.env.PORT || 3000);
+app.listen(port);
+console.log(`Server Listening on Port: ${port}`);
+
+
+// Logic
+app.get("/", (req, resp) =>
+{
+resp.sendFile( __dirname + "/index.html")
+});
