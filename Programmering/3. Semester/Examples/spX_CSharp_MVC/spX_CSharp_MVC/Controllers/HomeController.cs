8e67e1b8a94c42c48b9062977a154a81458ecb9a
Jonas Lykke <jona711p@student.eadania.dk>
2018-01-10 16:44:27 +0100

    C# Joke Example

---
+++ Programmering/3. Semester/Examples/spX_CSharp_MVC/spX_CSharp_MVC/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json.Linq;
+using spX_CSharp_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace spX_CSharp_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<Joke> joke;
+        static List<Joke> jokes = new List<Joke>();
+
+        public ActionResult Index()
+        {
+            joke = GetJokes();
+
+            ViewBag.jokes = jokes.OrderByDescending(x => x.TimeStamp);
+
+            return View(joke);
+        }
+        
+        public ActionResult SaveJoke(string id)
+        {
+            Joke inputJoke = new Joke();
+            inputJoke = joke.FirstOrDefault(x => x.ID == id);
+
+            jokes.Add(inputJoke);
+
+            return RedirectToAction("Index");
+        }
+
+        private List<Joke> GetJokes()
+        {
+            if (joke != null)
+            {
+                joke = null;
+            }
+
+            joke = new List<Joke>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                using (HttpResponseMessage response = client.GetAsync("https://api.chucknorris.io/jokes/random").Result)
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        HttpContent responseContent = response.Content;
+
+                        JObject jObject = JObject.Parse(responseContent.ReadAsStringAsync().Result);
+
+                        Joke chuckJoke = new Joke
+                        {
+                            ID = jObject["id"].ToString(),
+                            Origin = "Chuck Joke",
+                            Value = jObject["value"].ToString(),
+                            TimeStamp = DateTime.Now
+                        };
+
+                        joke.Add(chuckJoke);
+                    }
+                }
+
+                using (HttpResponseMessage response = client.GetAsync("https://icanhazdadjoke.com/").Result)
+                {
+                    client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        HttpContent responseContent = response.Content;
+
+                        JObject jObject = JObject.Parse(responseContent.ReadAsStringAsync().Result);
+
+                        Joke dadJoke = new Joke
+                        {
+                            ID = jObject["id"].ToString(),
+                            Origin = "Dad Joke",
+                            Value = jObject["joke"].ToString(),
+                            TimeStamp = DateTime.Now
+                        };
+
+                        joke.Add(dadJoke);
+                    }
+                }
+
+                return joke;
+            }
+        }
+    }
+}
---
+++ Programmering/3. Semester/Examples/spX_CSharp_MVC/spX_CSharp_MVC/Models/Joke.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace spX_CSharp_MVC.Models
+{
+    public class Joke
+    {
+        public string ID { get; set; }
+
+        [Display(Name = "Oprindelse")]
+        public string Origin { get; set; }
+
+        [Display(Name = "Joke")]
+        public string Value { get; set; }
+
+        [Display(Name = "Tidspunkt")]
+        public DateTime TimeStamp { get; set; }
+    }
+}
